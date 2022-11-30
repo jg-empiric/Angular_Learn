b7b13bc502d1faeed7f3a7d9708fbd914c0938d1
jg-empiric <jg.empiric@gmail.com>
2022-11-30 12:17:18 +0530

    made nestd loop

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,12 +6,11 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'switch case';
-  users = ['jenish', 'ghoghari', 'ishan', 'amit'];
-  usersDetails = [
-    {name: 'jenish', email: 'jenish@gmail.com', phone: '9999911111'},
-    {name: 'ishan', email: 'ishan@gmail.com', phone: '9999911112'},
-    {name: 'amit', email: 'amit@gmail.com', phone: '9999911113'},
-    {name: 'harsh', email: 'harsh@gmail.com', phone: '9999911114'},
+  title = 'nested for';
+  users = [
+    {name: 'jenish', email: 'jenish@gmail.com', phone: '9999911111', socialAccounts:['facebook', 'instagram', 'gmail']},
+    {name: 'ishan', email: 'ishan@gmail.com', phone: '9999911112', socialAccounts:['facebook', 'instagram', 'gmail']},
+    {name: 'amit', email: 'amit@gmail.com', phone: '9999911113', socialAccounts:['facebook', 'instagram', 'gmail']},
+    {name: 'harsh', email: 'harsh@gmail.com', phone: '9999911114', socialAccounts:['facebook', 'instagram', 'gmail']},
   ];
 }
