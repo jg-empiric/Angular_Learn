cb659bddd31963250c3926bad630e7720c33defc
jg-empiric <jg.empiric@gmail.com>
2022-11-30 11:24:26 +0530

    switch case

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,6 +6,6 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'if else';
-  color = "blue";
+  title = 'switch case';
+  color = "green";
 }
